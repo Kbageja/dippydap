679b691e46d71c4fd35e7f9a258cd4658621de8d
kinshuk bageja <kinshukbageja@gmail.com>
2026-09-19 01:10:11 +0530

    updated sitemap

--- dippydap/app/sitemap.ts
+++ dippydap/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const siteUrl = "https://dippydap.example.com";
+  const siteUrl = "https://dippydap.vercel.app";
   return [
     {
       url: siteUrl,
